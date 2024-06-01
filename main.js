7d91b4ba8a69936dbae39e4fb1fcec50f496a02f
AkshDZN <129135009+AkshDZN@users.noreply.github.com>
2024-06-01 11:40:29 +0530

    Added open and close functionality for pomodoro

--- main.js
+++ main.js
@@ -2,3 +2,16 @@ function hideWarning() {
     document.querySelector('.beta-warning').style.transform = 'translate(0%, -100%)';
 }
 
+let pomodoro = document.querySelector('.fullpomo');
+
+let pomoButtonState = 0;
+
+function pomoShowHide() {
+    if (pomoButtonState === 0) {
+        pomodoro.style.scale = "1";
+        pomoButtonState = 1;
+    } else if (pomoButtonState === 1) {
+        pomodoro.style.scale = "0";
+        pomoButtonState = 0;
+    }
+}
